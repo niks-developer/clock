5a7e3f7e01a1b081e2544fea9a5283638f1662f3
niks-developer <moyapo4ta_82@mail.ru>
2023-07-14 13:11:37 +0300

    обновление удаленного репозитория

---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+export default class Clock extends Component {
+constructor(props){
+    super(props);
+
+    this.state = {
+        time: new Date().toLocaleTimeString()
+    }
+}
+
+
+componentDidMount() {
+    this.timerID = setInterval(()=>
+        this.tick(), 1000)
+   
+}
+
+componentWillUnmount() {
+    clearInterval(this.timerID)
+    
+}
+
+tick() {
+    this.setState({
+        time: new Date().toLocaleTimeString()
+    })
+}
+
+render() {
+    return <p className="Time">{this.state.time}</p>
+   
+}
+
+}
